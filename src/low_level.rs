c6c0a36c682388c047d3538c89b18e54cc3bc09c
Matthew Maurer <matthew.r.maurer@gmail.com>
2015-08-24 17:29:55 -0400

    BAP Threading Interface

--- src/low_level.rs
+++ src/low_level.rs
@@ -3,27 +3,59 @@ use std::cell::RefCell;
 use std::sync::{Once, ONCE_INIT};
 use std::marker::PhantomData;
 
-#[allow(raw_pointer_derive)]
-#[derive(Clone)]
 pub struct Context {
   stamp: PhantomData<*const Context>
 }
 
-thread_local!(static BAP_CTX: RefCell<Option<Context>> = RefCell::new(None));
+impl Drop for Context {
+  fn drop(&mut self) {
+    unsafe {assert!(raw::bap_thread_unregister())}
+  }
+}
 
+thread_local!(static BAP_CTX : RefCell<Option<Context>> = RefCell::new(None));
+thread_local!(static BAP_THREAD_REGISTER : Once = ONCE_INIT);
 static BAP_INIT : Once = ONCE_INIT;
 
-pub fn init() -> Option<Context> {
-  BAP_INIT.call_once(|| {
-    unsafe {
-      raw::bap_init()
+pub fn with_bap<A, F>(f : F) -> A
+  where F : Fn(&Context) -> A {
+  BAP_THREAD_REGISTER.with(|t| {
+    let t = unsafe {
+      //with has too tight a bound on the functions, cast the reference
+      ::std::mem::transmute::<&Once, &'static Once>(t)
     };
-    BAP_CTX.with(|ctx| {
-      *ctx.borrow_mut() = Some(Context { stamp : PhantomData });
+    t.call_once(|| {
+      BAP_INIT.call_once(|| {
+        unsafe {
+          raw::bap_init();
+          raw::bap_release();
+        }
+        BAP_CTX.with(|ctx| {
+          *ctx.borrow_mut() = Some(Context { stamp : PhantomData });
+        })
+      });
+      BAP_CTX.with(|ctx| {
+        let mut r = ctx.borrow_mut();
+        match *r {
+          Some(_) => (),
+          None => {
+            unsafe {assert!(raw::bap_thread_register())};
+            *r = Some(Context { stamp : PhantomData })
+          }
+        }
+      })
     })
   });
   BAP_CTX.with(|ctx| {
-    (*ctx.borrow()).clone()
+    match *ctx.borrow() {
+      Some(ref c) => {
+        unsafe {raw::bap_acquire()};
+        let r = f(c);
+        unsafe {raw::bap_release()};
+        r
+      },
+      None    => panic!("Empty context?")
+    }
   })
 }
 
@@ -50,8 +82,8 @@ abs_type!(insn, bap_insn, bap_free_insn, Instruction);
 abs_type!(bigstring, bap_bigstring, bap_free_bigstring, BigString);
 
 pub enum Endian {
-  LittleEndian,
-  BigEndian
+  Little,
+  Big
 }
 
 #[allow(non_camel_case_types)]
@@ -158,8 +190,13 @@ pub enum Stmt {
               else_clause : Vec<Stmt>}
 }
 
-impl ToString for BitVector {
-  fn to_string(&self) -> String {
+impl BitVector {
+  pub fn create_64(_ctx : &Context, val : u64, width : BitSize) -> Self {
+    unsafe {
+      BitVector {raw : raw::bap_create_bitvector64(val as i64, width as i16)}
+    }
+  }
+  pub fn to_string(&self, _ctx : &Context) -> String {
     unsafe {
       use std::ffi::CStr;
       use libc::types::common::c95::c_void;
@@ -172,16 +209,59 @@ impl ToString for BitVector {
   }
 }
 
-impl BitVector {
-  pub fn create_64(_ctx : &Context, val : u64, width : BitSize) -> Self {
-    unsafe {
-      BitVector {raw : raw::bap_create_bitvector64(val as i64, width as i16)}
+impl BigString {
+  pub fn new(_ctx : &Context, buf : &[u8]) -> BigString {
+    BigString {
+      raw : unsafe {
+        raw::bap_create_bigstring(buf.as_ptr(),
+                                  buf.len() as raw::bindings_compat::size_t)
+      }
+    }
+  }
+}
+
+impl Endian {
+  fn to_bap(&self) -> raw::bap_endian {
+    match *self {
+      Endian::Little => raw::BAP_LITTLE_ENDIAN,
+      Endian::Big    => raw::BAP_BIG_ENDIAN
+    }
+  }
+}
+
+impl MemRegion {
+  pub fn new(_ctx : &Context,
+             bs : &BigString,
+             off : usize, len : usize,
+             endian : Endian, addr : &Addr) -> MemRegion {
+    use raw::bindings_compat::size_t;
+    MemRegion {
+      raw : unsafe {
+        raw::bap_create_mem(off as size_t,
+                            len as size_t,
+                            endian.to_bap(),
+                            addr.raw,
+                            bs.raw)
+      }
     }
   }
 }
 
 #[test]
 fn create_and_print_bitvector() {
-  assert_eq!(&BitVector::create_64(&init().unwrap(), 37, 9).to_string(),
-             "0x25:9")
+  with_bap(|ctx| {
+    assert_eq!(&BitVector::create_64(ctx, 37, 9).to_string(ctx),
+               "0x25:9")
+  })
+}
+
+#[test]
+fn create_and_print_mem() {
+  with_bap(|ctx| {
+    let base = BitVector::create_64(ctx, 32, 64);
+    let shell = b"\x31\xc0\x50\x68//sh\x68/bin\x89\xe3\x50\x53\x89\xe1\x99\xb0\x0b\xcd\x80";
+    let bs = BigString::new(ctx, shell);
+    let mem = MemRegion::new(ctx, &bs, 0, shell.len(), Endian::Little, &base);
+    ()
+  })
 }
